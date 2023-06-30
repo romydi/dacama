30cf6c53037d17b37f92e1da4513d7c2820da060
romydi <rominadiaz87@gmail.com>
2023-06-30 17:55:36 -0300

    se sube login sin back

---
+++ dacama/Login.cs
@@ -0,0 +1,15 @@
+namespace dacama
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
